2527f7808eae0050cc6ab705af82f253181eddb2
Alone-wl <18234028271@163.com>
2019-04-25 23:40:43 +0800

    add README.md .gitignore

---
+++ src/hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+
+    std::cout << "Hello World!" << std::endl;
+    system("pause");
+
+    for (int i = 0; i <= 10; i++)
+    {
+        
+       }
+
+    
+    
+
+    return 0;
+}
